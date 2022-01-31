aff9931d91cdd42b6445696f7a87567d58d76781
Amarachi <49257104+Marycynthia-mara@users.noreply.github.com>
2022-01-31 16:35:38 +0100

    optimizing

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// // require('./bootstrap');
+
+// window.onload=function(){
+        
+//     // ***sidebar/navbar and menubar *** //
+
+//     // grab everything we need
+//     const btn = document.querySelector(".mobile-menu-button");
+//     const overlay = document.querySelector(".overlay");
+//     const sidebar = document.querySelector(".sidebar");
+
+//     // add our event listener for the click
+//     btn.addEventListener("click", () => {
+//         sidebar.classList.toggle("translate-hide");
+//         overlay.classList.toggle("hidden");
+//     });
+
+//     overlay.addEventListener("click", () => {
+//         sidebar.classList.toggle("translate-hide");
+//         overlay.classList.toggle("hidden");
+//     });
+    
+//   }
+
